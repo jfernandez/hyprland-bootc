dc1260b565417e28a0ee7c9bd9a52707645d3701
Jose Fernandez <me@jrfernandez.com>
2025-12-08 19:01:18 -0700

    fix: create system user/groups from sysusers.d configs explicitely

--- build_files/scripts/enable-services.sh
+++ build_files/scripts/enable-services.sh
@@ -5,3 +5,7 @@ set -ouex pipefail
 
 # Display manager
 systemctl enable sddm.service
+
+# Create system users/groups from sysusers.d configs
+# Required because systemd doesn't run during container build
+systemd-sysusers
